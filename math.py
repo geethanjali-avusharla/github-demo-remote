6c13066f3c7eee20e74a9d6621ae737c2e2dc74e
geethanjali-avusharla <101399308+geethanjali-avusharla@users.noreply.github.com>
2022-03-11 17:20:40 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,20 @@
+# Add implementation
+def add(x,y):
+    return x+y;
+# Subtract implementation
+def subtract(x,y):
+    return x-y            #on master
+# multiply implementation
+def multiply(x,y):
+    return x*y          #on bug456
+# divide implementation
+def divide(x,y):
+    if y==0:            #on master
+        return DIVIDE_BY_0_ERROR;
+    else:
+        return x/y
+
+#square implntn
+def square(x):
+    return x*x
+
