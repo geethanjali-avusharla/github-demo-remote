cf59f506e82026c81daecac14cecdf9d03c974c0
geetha <avusharlageethanjali@gmail.com>
2022-03-11 17:26:51 +0530

    added fn headers

--- math.py
+++ math.py
@@ -1,20 +1,20 @@
-# Add implementation
+# Add implementation1
 def add(x,y):
     return x+y;
-# Subtract implementation
+# Subtract implementation1
 def subtract(x,y):
     return x-y            #on master
-# multiply implementation
+# multiply implementation1
 def multiply(x,y):
     return x*y          #on bug456
-# divide implementation
+# divide implementation1
 def divide(x,y):
     if y==0:            #on master
         return DIVIDE_BY_0_ERROR;
     else:
         return x/y
 
-#square implntn
+#square implntn1
 def square(x):
     return x*x
 
